137eb3a2d88169507c15e1bd65d3080a8b8b4229
Vincent Kautz <vk@ccontor.zzz>
2022-11-15 16:29:41 +0100

    Redo of player class value checking

--- scripty.py
+++ scripty.py
@@ -13,32 +13,42 @@ class Player:
       Player.counter += 1
   
     def set_name(self):
-      result = input("Player " + str(Player.counter) + " choose your player name (only alphanumeric): ")  
-      while True:
+      result = None  
+      while result is None: 
+        input_value = input("Player " + str(Player.counter) + " choose your player name (only alphanumeric): ")  
         #Testing if Input okay
-        if len(result) == 0:
-          result = input("Input cannot be empty. Please enter a string: ")
+        if len(input_value) == 0:
+          input_value = input("Input cannot be empty. Please enter a string: ")
           continue
 
-        if not result.isalnum():
+        if not input_value.isalnum():
           #checking input
-          result = input("Input cannot be non-alphanumeric. Please enter a string: ")
-          continue
-        else:
-          #Value is okay 
-          break  
-      return result
-
-    def set_symbol(self):
-      result = input(input("Player " + self.player_name + " please enter a single letter as your Symbol: "))
-      #Testing if Input okay
-      while True:
-        if not result.isalpha() or not len(result) == 1:
-          #checking input
-          result = input("Input cannot be non-alpha or contain more than one character. Please enter a letter: ")
+          input_value = input("Input cannot be non-alphanumeric. Please enter a string: ")
           continue
         else:
           #Value is okay
+          result = input_value 
+          break  
+      return result
+
+
+
+    def set_symbol(self):
+      result = None
+      while result is None:
+        input_value = input(self.player_name + " please enter a single letter as your Symbol: ")
+      #Testing if Input okay
+        if len(input_value) != 1:
+          input_value = input("Input cannot be non-alpha or contain more than one character. Please enter a single letter: ")
+          continue
+
+        if not input_value.isalpha():
+          #checking input
+          input_value = input("Input cannot be non-alpha. Please enter a single letter: ")
+          continue
+        else:
+          #Value is okay
+          result = input_value
           break  
       return result
 
