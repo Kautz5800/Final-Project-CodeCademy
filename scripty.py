ebcb2e2269307e91a6f34a09e474f149fedfd7ea
Vincent Kautz <vk@ccontor.zzz>
2022-11-15 15:43:31 +0100

    2. Commit
    created class check for name and symbol

--- scripty.py
+++ scripty.py
@@ -1,17 +1,46 @@
 # key only dict for the Tic-tac-toe
-class Game():
-    def __init(self)__:
-        game_field = {1:"", 2:"", 3:"", 4:"", 5:"",6:"",7:"",8:"",9:"",}
+class Game:
+    def __init__(self):
+      self.game_field = {1:"", 2:"", 3:"", 4:"", 5:"",6:"",7:"",8:"",9:"",}
 
 class Player:
     counter = 1
 
     def __init__(self):
       self.player_symbol = ""
-      self.player_name = input("Player " + str(Player.counter) + " choose your player name:")
-      self.player_symbol = input("Player " +self.player_name+" choose your Symbol: ")
+      self.player_name = self.set_name()
+      self.player_symbol = self.set_symbol()
       Player.counter += 1
+  
+    def set_name(self):
+      result = input("Player " + str(Player.counter) + " choose your player name (only alphanumeric): ")  
+      while True:
+        #Testing if Input okay
+        if len(result) == 0:
+          result = input("Input cannot be empty. Please enter a string: ")
+          continue
 
+        if not result.isalnum():
+          #checking input
+          result = input("Input cannot be non-alphanumeric. Please enter a string: ")
+          continue
+        else:
+          #Value is okay 
+          break  
+      return result
+
+    def set_symbol(self):
+      result = input(input("Player " + self.player_name + " please enter a single letter as your Symbol: "))
+      #Testing if Input okay
+      while True:
+        if not result.isalpha() or not len(result) == 1:
+          #checking input
+          result = input("Input cannot be non-alpha or contain more than one character. Please enter a letter: ")
+          continue
+        else:
+          #Value is okay
+          break  
+      return result
 
 player_one = Player()
 player_two = Player()
