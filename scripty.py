59db279968019a126e3b9c52e6d3ca5d193d5bc4
Vincent Kautz <vk@ccontor.zzz>
2022-11-25 09:15:24 +0100

    Changes to the playbook / Updateting the comments in code

--- scripty.py
+++ scripty.py
@@ -3,13 +3,26 @@ from random import randrange
 
 # key only dict for the Tic-tac-toe
 class Game:
+  # initiation of the game class
+  # the game_field directiory contains spaces, so the output keeps the same outlines
+  # current_player is an "empty" int, so it can changed depending on the Player Class variable counter
+  # finished Boolean if the game is won or finished without a winner  
     def __init__(self):
       self.game_field = {1:" ", 2:" ", 3:" ", 4:" ", 5:" ",6:" ",7:" ",8:" ",9:" "}
       self.finished = False
       self.current_player = int()
-
+    
+    #created for a test 
     def __repr__(self):
       return "A Game of Tic-Tac-Toe!"
+    
+    # testing all possible ways to win the game
+    # checking the rows after the following pattern:
+    #   first field in a possible winning condition against the second field
+    #   then the second against the third field
+    #   at last if the third isn't the empty string out of the Game Class __init__()
+    # sets the variable won to True if one of the condtions is met
+    # the result is returned to the Class method print_playing_field  
 
     def check_win(self):
       won = False
@@ -31,6 +44,11 @@ class Game:
         won = True
       self.finished = won
       return won
+    
+    # basicly just print the imputs to the console
+    # additonaly calls the method check_win see above
+    # if none of the original "space"-values of the game_field exits 
+    # the game is gonna end without a winner  
 
     def print_playing_field(self):
       print("")
@@ -43,14 +61,24 @@ class Game:
       if Game.check_win(self):
         print("Games ends with one Winner!")
         self.finished = True
-        exit()   
+        exit()
+      elif " " not in self.game_field.values():
+      # elif all(value != " " for value in self.game_field.values()):
+        print("Games ends without a Winner!")
+        self.finished = True
+        exit()     
+    
+    # method for checking the inputs to the playing field
+    # asking for the value of the field which a player is gonna choose  
+    # using a while loop to check if a valid input is used
+    # if a valid value is used call method print_playing_field    
 
     def player_input(self, player):
       self.current_player = player.counter
       result = None
       input_value = input("Player "+ player.player_name + " choose your Field:")
       while result is None:
-        if game.game_field[int(input_value)] != " ":
+        if int(input_value) not in range(1, 10) or game.game_field[int(input_value)] != " ":
           input_value = input("Player "+ player.player_name + " choose your Field:")
           continue
         else:
